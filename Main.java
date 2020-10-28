a15ec79607aae658bb3bcf69a76e64d007d17aa6
Vaibhav Mahajan <vaibhavmahajan124@gmail.com>
2020-10-28 22:23:19 +0530

    My first Commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // The first frame of this software that is LOGINSCREEN
+        LoginScreenFinal t = new LoginScreenFinal();
+        t.setVisible(true);
+        //ManagerScreen m = new ManagerScreen();
+        //m.setVisible(true);
+       //EmpScreen e= new EmpScreen();
+       //e.setVisible(true); 
+    }
+}
